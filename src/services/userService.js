b09c20f57c1cddfd78a56f955888430ab8fb1227
guyera <thegraygrimm@graymansurvivaltech.com>
2025-08-26 14:23:34 -0400

    deleted / from service BASE_URLS

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,6 +1,6 @@
 // src/services/userService.js
 
-const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/users`;
+const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}users`;
 
 const index = async () => {
     try {
