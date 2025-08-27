e46579a6300228e58d0ec0acaeb779a11506231c
Shaina G <shainagtherapy@gmail.com>
2025-08-27 12:22:53 -0700

    Merge conflict changes

--- src/components/PlaylistList/PlaylistList.jsx
+++ src/components/PlaylistList/PlaylistList.jsx
@@ -3,12 +3,13 @@ import { Link } from 'react-router';
 
 const PlaylistList = (props) => {
     return ( 
-    <main>
-        {props.playlists.map((playlist) => (
-            <Link key={playlist._id} to={`/playlists/${playlist._id}`}> 
-            <p key={playlist._id}>{playlist.playlist}</p> </Link>
-        ))}
-    </main>
+        <main>
+            {props.playlists.map((playlist) => (
+                <Link key={playlist._id} to={`/playlists/${playlist._id}`}> 
+                    <p key={playlist._id}>{playlist.playlist}</p>
+                </Link>
+            ))}
+        </main>
     );
 };
 
